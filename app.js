012f3429f375347433a1e279dd4f63d9c30f6c8c
Parshvavyas17 <parshvavyas07@gmail.com>
2022-06-19 02:07:08 +0530

    Made first http request

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=f919f71a20db63416b24723fd7f0de8f&query=37.8267,-122.4233'
 
-setTimeout(() => {
-    console.log('0 second timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
